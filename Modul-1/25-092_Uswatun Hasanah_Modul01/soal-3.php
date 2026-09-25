95b7e718857d772017dd8d245f2db727e798fa04
uswahmygz-jpg <uswa.myg.z@gmail.com>
2026-09-25 15:27:36 +0700

    Modul01

---
+++ Modul-1/25-092_Uswatun Hasanah_Modul01/soal-3.php
@@ -0,0 +1,17 @@
+<!-- Soal 1 -->
+<?php
+// ini non embedded script
+echo "Hello World";
+?>
+<!-- Soal 2 -->
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// ini embedded script
+echo "Hello World";
+?>
+
+</body>
+</html>
